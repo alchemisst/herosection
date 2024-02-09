c10d912bea663ad4bb66184370a4aa14d559aeb7
storycode143 <storycode143@gmail.com>
2024-02-09 11:06:20 +0530

    customized

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -43,7 +43,7 @@ function NavBar() {
     <Navbar expand="lg"  className={scrolled ? 'scrolled' : '' }>
       <Container>
         <Navbar.Brand href="#home" className='navbar-logo'>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logoIMG" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" >
           <span className='navbar-toggler-icon'></span>
